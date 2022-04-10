d5541871be23adbe798fd5383b41ec026728512e
keiichi031605 <keichi031605@gmail.com>
2022-04-11 08:10:03 +1000

    second commit

---
+++ src/design_by_contract/StudentEnrolment.java
@@ -0,0 +1,37 @@
+package design_by_contract;
+import java.util.*;   
+
+public class StudentEnrolment {
+  private final Set<String> coresOffered = new HashSet<String>( Arrays.asList("P1", "OS", "SEF", "DC", "WP"));
+  private final Set<String> electivesOffered
+      = new HashSet<String>
+  (Arrays.asList("C++", "PHP", "Web3D", "IT")); private Set<String> electives = new HashSet<String>(); private Set<String> cores = new HashSet<String>();
+  // adding a core
+  public void addCore(String core)
+  {
+    cores.add(core);
+  }
+  // adding an elective
+  public void addElective(String elective)
+  {
+    electives.add(elective);
+  }
+  // RMIT University 2020
+  // School of Science 34
+  // removing a core
+    public void removeCore(String core)
+  {
+        cores.remove(core);
+    }
+    // removing an elective
+    public void removeElective(String elective)
+  {
+        electives.remove(elective);
+    }
+    // listing all courses enrolled
+    public void display()
+  {
+    for (String course : cores) System.out.println(course);
+    for (String course : electives) System.out.println(course);
+  }
+}
