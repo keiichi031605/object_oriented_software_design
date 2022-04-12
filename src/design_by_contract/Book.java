5725c1a0953fcaaeea91582cfb93a527b8f69107
keiichi031605 <keichi031605@gmail.com>
2022-04-12 13:43:12 +1000

    created book and soppingCart class

---
+++ src/design_by_contract/Book.java
@@ -0,0 +1,44 @@
+package design_by_contract;
+
+import com.google.java.contract.*;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+ 
+ 
+@Invariant({"title != null && title.length() > 0", "price > 0"})
+public class Book {
+    private final String title;
+    private int price;
+ 
+    @Requires({"title != null && title.length() > 0", "price > 0"})
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+ 
+    public int getPrice() {
+        return price;
+    }
+ 
+    @Requires("price > 0")
+    public void setPrice(int price) {
+        this.price = price;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    @Override
+    public boolean equals(Object obj) {
+        // boilerplate implementation from Apache Commons Lang
+        return EqualsBuilder.reflectionEquals(this, obj, "price");
+    }
+ 
+    @Override
+    public int hashCode() {
+        // boilerplate implementation from Apache Commons Lang
+        return HashCodeBuilder.reflectionHashCode(this, "price");
+    }
+}
