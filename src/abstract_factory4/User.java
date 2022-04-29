422dcb9c40c0dda708a241ce56b7f0054522ff83
keiichi031605 <keichi031605@gmail.com>
2022-04-29 15:42:41 +1000

    exersiced abstract factory

---
+++ src/abstract_factory4/User.java
@@ -0,0 +1,14 @@
+package abstract_factory4;
+
+import java.util.*;
+
+public class User{ 
+//	public static void main(String args[]){ 
+//        HotPot hotPot = new HotPot(new Pot()); 
+//        Factory factory = new MizutakiFactory(); 
+//        hotPot.addSoup(factory.getSoup()); 
+//        hotPot.addMain(factory.getMain()); 
+//        hotPot.addVegetables(factory.getVegetables()); 
+//        hotPot.addOtherIngredients(factory.getOtherIngredients());
+//    } 
+}
