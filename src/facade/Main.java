8820c85f7cba43b88a0de9cef60172f5e242ab3e
keiichi031605 <keichi031605@gmail.com>
2022-05-03 15:29:05 +1000

    exersiced structural pattern

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -1,8 +1,8 @@
 package facade;
 
+//https://qiita.com/i-tanaka730/items/911604bcd4136684ff1c
 public class Main {
     public static void main(String[] args) {
         PageMaker.makeWelcomePage("tanaka@test.com", "welcome.html");
     }
 }
-//https://qiita.com/i-tanaka730/items/911604bcd4136684ff1c
